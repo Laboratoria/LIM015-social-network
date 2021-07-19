f834b63a2eac273bf20a7a55eddf23c333be6f19
RocioSulca <rociosulca.z@gmail.com>
2021-07-19 11:53:45 -0500

    First commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 // Este es el punto de entrada de tu aplicacion
-
 import { myFunction } from './lib/index.js';
-
 myFunction();
+
+//document.getElementById("signIn").onclick = () => (window.open("_blank"));
+
+const signIn = document.querySelector("#signIn");
+
+signIn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+
+    auth.createUserWithEmailAndPassword(username, password)
+        .then((userCredential) => {
+            // Signed in
+            let user = userCredential.user;
+            console.log(user);
+        })
+
+        
+    //console.log(username, password);
+});
