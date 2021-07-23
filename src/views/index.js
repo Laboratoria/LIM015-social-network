8cbfb48336789cfa43301dcc3daf857213745a56
JackelinGM <gutmontemel@gmail.com>
2021-07-22 20:39:42 -0500

    funciomnamiento de views

---
+++ src/views/index.js
@@ -0,0 +1,12 @@
+import Singin from './singin.js';
+import Singup from './singup.js';
+import Home from './home.js';
+import Profile from './profile.js';
+
+export const conponents = {
+    singin : Singin,
+    singup : Singup,
+    home  : Home,
+    profile : Profile
+}
+
