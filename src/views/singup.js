09f61d1c5e3d86daa314679b10a2dfa8601ffa0d
Alemapyapur <pamela.rupay31@gmail.com>
2021-07-24 09:32:47 -0500

    correcion en singin

--- src/views/singup.js
+++ src/views/singup.js
@@ -1,19 +1,16 @@
 export default () => {
-    const sectionElem = document.createElement('section');
-    const template = `
-    <section id="contenedorView1" class="contenedorView1">
+  const sectionElem = document.createElement('section');
+  const template = `
+  <section id="contenedorView1" class="contenedorView1">
     <input type="text" class="registro" id="nombres" placeholder=" Ingresar Nombres">
     <input type="text" class="registro" id="apellidos" placeholder=" Ingresar Apellidos">
-    <input type="text" class="registro" id="email" placeholder=" Ingresar Correo Electronico">
-    <input type="text" class="registro" id="password" placeholder=" Ingresar Contraseña">
+    <input type="email" class="registro" id="email" placeholder=" Ingresar Correo Electronico">
+    <input type="password" class="registro" id="password" placeholder=" Ingresar Contraseña">
     <input type="submit" href="#/" class="registro" value="Registrar">
-    </section>
-    `;
+  </section>
+  `;
 
-    sectionElem.innerHTML = template;
+  sectionElem.innerHTML = template;
 
-    return sectionElem;
+  return sectionElem;
 };
-
-
-
