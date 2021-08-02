d0956ce57da6c428245fa6241d33c801ac99a756
alissonsegura <alissonicoleseguratarazona@gmail.com>
2021-08-01 22:53:12 -0500

    add login sig up and firebase dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-// Este es el punto de entrada de tu aplicacion
-
-import { myFunction } from './lib/index.js';
-
-myFunction();
+// // Este es el punto de entrada de tu aplicacion
+// import { myFunction } from './lib/index.js';
+// myFunction();
+// const loginForm = document.getElementById('login-form');
+// loginForm.addEventListener('submit', () => {
+//     console.log('Hello');
+// });
