f67e4afe1e7b2b5b6a55dbd2af475843630d9d9a
nadia <nadia.chayna.q@gmail.com>
2021-08-03 16:16:02 -0500

    firebase sign up

--- src/firebaselogin.js
+++ src/firebaselogin.js
@@ -5,12 +5,10 @@ signupForm.addEventListener('submit', (e) => {
   const user = document.getElementById('signup-user').value;
   const email = document.getElementById('signup-email').value;
   const password = document.getElementById('signup-password').value;
+ console.log(user, email, password);
   // debajo de "auth" colocamos un método de Firebase, que va a permitirnos enviar los datos.
   // lo que capturamos en el value, le brindando a Firebase para que pueda validarlo.
-  firebase.auth()
-    .createUserWithEmailAndPassword(user, email, password)
-    // "then" va a devolver una respuesta, que son las credenciales del usuario
-    .then(userCredential => {
-      console.log('sign up'); // signupForm.reset(); // limpiar el formulario
-    });
+  auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
+    console.log('sign up'); // limpiar el formulario
+  });
 });
