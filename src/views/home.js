0b6764375c660b63c840f3d17b1e96c7ea8debdd
JackelinGM <gutmontemel@gmail.com>
2021-08-03 21:09:44 -0500

    post y comentarios

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,7 @@
 // Inicio de las publicaciones
 import { sharePost } from '../firebase/data-base.js';
+import { sharingPost } from '../views/post.js';
+import { currentUser } from '../firebase/autenticacion.js';
 
 export default () => {
   const sectionHome = document.createElement('section');
@@ -12,7 +14,7 @@ export default () => {
                 <nav class="nav-bar" id="nav">
                     <ul>
                         <li><a href="#/home">Home</a></li>
-                        <li><a href="#/profile">Profile</a></li>
+                        <li class="current-user"><a href="#/profile">${currentUser().displayName}</a></li>
                         <li><a href="#/">Sign-Out</a></li>
                     </ul>
                 </nav>
@@ -20,15 +22,11 @@ export default () => {
         </section>
             <section class = "container-perfil">
             <section id="user-perfil" class="user-perfil">
-                <section>
-                    <img class="img-profile" src="img/libro9.png" alt="">
-                </section>
+                <img class="img-profile" src="img/libro9.png" alt="">
+                    <img class="img-perfil" src='${currentUser().photoURL}'/>
+                    <p class="email-perfil">${currentUser().email}</p>
                 <section>
                     <section class="flex margin">
-                        <section class="flex-align">
-                            <p>jala la foto gmail en img en vez de p</p>
-                            <h2 id="profile-name">jala el nombre de gmail</h2>
-                        </section>
                         <hr>
                         <section class="margin">
                             <section class="flex-align">
@@ -46,8 +44,8 @@ export default () => {
 <!--Estos son para que las personas puedan escribir sus post-->
     <section class="content-post">
         <section class="form-save">
-            <form class="padding" id="container-form" maxlength="50" required>
-                <textarea placeholder="¿Que quieres compartir?" id= "share-post" class="textArea-comment "></textarea>
+            <form class="padding" maxlength="50" required>
+                <textarea placeholder="¿Que quieres compartir?" class="textArea-comment "></textarea>
                     <section class="flex-bottom-form">
                         <section>
                             <label for="fileButton">
@@ -56,40 +54,23 @@ export default () => {
                             </label>
                             <input type="file" class="hide">
                         </section>
-                        <select class="btn-select" id="privacy">
+                        <select class="btn-select">
                             <option value="publico" selected>Público</option>
                             <option value="privado" select>Privado</option>
                         </select>
-                        <input type="button" id="btn-share" class="btn-share" value="compartir">
+                        <input type="button" class="btn-share" value="compartir">
                     </section>
                 </form>
             </section>
     </section>
-    </section>
+</section>
+<section id="container-post"></section>
     `;
 
   sectionHome.innerHTML = template;
   sectionHome.setAttribute('class', 'contenedor-Home');
-
-  const containerForm = document.querySelector('#container-form');
-  const btnShare = document.querySelector('#btn-share');
-  // para publicar los post
-  // btnShare.addEventListener('click', sharePost);
-  console.log(btnShare);
-  containerForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const post = btnShare['share-post'];
-    // const email = btnShare['share-email'];
-    // const nombre = btnShare['share-nombre'];
-    // const date = btnShare['share-date'];
-    // const like = btnShare['share-like'];
-    // const privacy = btnShare['privacy'];
-
-    sharePost(post.value);
-
-    btnShare.reset();
-    post.focus();
-  });
+  const containerPost = document.getElementById('container-post');
+  containerPost.appendChild(sharingPost);
+  console.log(containerPost);
   return sectionHome;
 };
