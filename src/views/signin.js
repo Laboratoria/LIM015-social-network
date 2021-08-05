997d49fa414faa011c8bd1fe0c0017f17d75da1d
JackelinGM <gutmontemel@gmail.com>
2021-08-05 13:10:07 -0500

    css en home

--- src/views/signin.js
+++ src/views/signin.js
@@ -3,29 +3,29 @@ import { signIn, signInWithGoogle, signInFb } from '../views-controllers/signin-
 export default () => {
   const sectionSignIn = document.createElement('section');
   const template = `
-    <section id="contenedorSignIn" class="contenedorSignIn">
-    <section class="side">
-      <p>BIENVENIDOS</p>
-      <img class="ibook-img" src="img/ibook-img.png" width="300px" alt="imagen relacionado a ibook"/>
-      <p>"Si quieres aventura, lanzate a la lectura"</p>
-    </section>
-      <!-- Seccion donde esta el formulario de SignIn -->
-      <form class="data-container">
-          <img class="logo-ibook" src="img/logo-ibook.png" width="200px" alt="logo de iBook"/>
-          <input type="email" class="registro" id="username" placeholder=" E-mail" required>
-            <input type="password" class="registro" id="password1" placeholder=" Password" required>
-            <input type="button" class="registro-signin" href="#/home" id="sign-in" value="Sign-In">
-            <p class="error" id="error"></p>
-            <a href="#" class="btn-forget">¿Olvidaste tu contraseña?</a>
-          <p class="text-login">Ingresar por</p>
-      <!-- Seccion donde estan los botones -->
-        <section class="btn-fb-google">
-          <button id="btn-fb" class="button"><i class="fab fa-facebook"></i></button>
-          <button id="btn-gg" class="button"><i class="fab fa-google"></i></button>
-        </section>
-        <label class="registrate">¿No tienes una cuenta?&nbsp;<a class="btn-register" href="#/signup" id="registrate">Regístrate.</a></label>
-      </form>
-    </section>
+  <section id="contenedorSignIn" class="contenedorSignIn">
+  <section class="side">
+    <p class="text-welcome">BIENVENIDOS</p>
+    <img class="ibook-img" src="img/ibook-img.png" width="300px" alt="imagen relacionado a ibook"/>
+    <p class="text-text">"Si quieres aventura, lanzate a la lectura"</p>
+  </section>
+    <!-- Seccion donde esta el formulario de SignIn -->
+    <form class="data-container">
+        <img class="logo-ibook" src="img/logo-ibook.png" width="200px" alt="logo de iBook"/>
+        <input type="email" class="registro" id="username" placeholder=" E-mail" required>
+          <input type="password" class="registro" id="password1" placeholder=" Password" required>
+          <input type="button" class="registro-signin" href="#/home" id="sign-in" value="Sign-In">
+          <p class="error" id="error"></p>
+          <a href="#" class="btn-forget">¿Olvidaste tu contraseña?</a>
+        <p class="text-login">Ingresar por</p>
+    <!-- Seccion donde estan los botones -->
+      <section class="btn-fb-google">
+        <button id="btn-fb" class="button"><i class="fab fa-facebook"></i></button>
+        <button id="btn-gg" class="button"><i class="fab fa-google"></i></button>
+      </section>
+      <label class="registrate">¿No tienes una cuenta?&nbsp;<a class="btn-register" href="#/signup" id="registrate">Regístrate.</a></label>
+    </form>
+  </section>
     `;
 
   // agregar clase para dar css
