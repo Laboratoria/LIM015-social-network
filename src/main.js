8fbbf5bd4508479e06cddfae8bcf167110f394a1
Andrea Blanco <estefania_8_3@hotmail.com>
2021-08-05 15:20:59 -0500

    agregue los archivos login.js y signIn.js

--- src/main.js
+++ src/main.js
@@ -7,23 +7,6 @@ const headerTag = document.getElementById('header');
 const contentPrincipal = document.getElementById('content');
 const footerTag = document.getElementById('footer');
 
-// Seccion de login
-const formSection = document.createElement('form');
-contentPrincipal.appendChild(formSection);
-const emailTag = `<div>
-<input type='email' id='email' class='inputLogin' placeholder='Email' required/>
-</div>`;
-const inputPassword = `<div>
-<input type='password' id='pass' class='inputLogin' placeholder='Password' required/>
-</div>`;
-const buttonSubmit = `<div>
-<button type="submit" class='buttonLogin'>Submit</button>
-</div>`;
-formSection.innerHTML = `${emailTag}${inputPassword}${buttonSubmit}`;
-// ACCION DEL LOGIN
-formSection.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const userEmail = document.getElementById('email').value;
-  const userPass = document.getElementById('pass').value;
-  // console.log(userEmail, userPass);
-});
+
+
+contentPrincipal.innerHTML = login();
