659710f9f5bd766a16f1e9b6c0c794af56d192c1
nadia <nadia.chayna.q@gmail.com>
2021-08-05 09:10:08 -0500

    cambio del nombre de la página

--- src/firebaselogin.js
+++ src/firebaselogin.js
@@ -10,36 +10,3 @@ const firebaseConfig = {
   measurementId: 'G-J7PWEV55T7',
 };
   // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-// cada vez que le enviamos datos a Firebase, le pedimos que lo valide con firebase.auth
-const fs = firebase.firestore();
-firebase.analytics();
-const signupForm = document.querySelector('.sign-up-form');
-
-signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const user = document.getElementById('signup-user').value;
-  const email = document.getElementById('signup-email').value;
-  const password = document.getElementById('signup-password').value;
-  console.log(user, email, password);
-  // debajo de "auth" colocamos un método de Firebase, que va a permitirnos enviar los datos.
-  // lo que capturamos en el value, le brindando a Firebase para que pueda validarlo.
-  auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
-    console.log('sign up');
-  });
-});
-
-// SING IN
-const signinForm = document.querySelector('.sign-in-form');
-
-signinForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('signin-email').value;
-  const password = document.getElementById('signin-password').value;
-  console.log(email, password);
-
-  auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
-    console.log('sign in');
-  });
-});
