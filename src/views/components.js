7aaeb4c0aa2c5c7bf7bfd7783e45efc4c1be8ba8
mysticBel <dnxbel@gmail.com>
2021-08-07 18:01:25 -0500

    agrege vistas

---
+++ src/views/components.js
@@ -0,0 +1,13 @@
+import Profile from './profile.js';
+import MainPage from './appMainPage.js';
+
+
+
+const components = {
+    profile: Profile,
+    mainPage: MainPage
+
+ }
+
+
+export { components }
