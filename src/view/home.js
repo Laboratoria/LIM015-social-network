746123e3b1a1a98730d5e18bf2189b9332149edd
Gina <floresgina2020@gmail.com>
2021-08-10 09:31:29 -0500

    rutas y vistas

--- src/view/home.js
+++ src/view/home.js
@@ -1,12 +1,31 @@
-import { navigator } from './allTemplate.js';
-
 export const home = () => {
-  const sectionHome = document.createElement('div');
+  const sectionHome = document.createElement('section');
+  // const nodo = document.createElement('div');
   sectionHome.classList.add('iHome');
-  const templateHome = `
+  sectionHome.innerHTML = `
   <h1>HOME</h1>
   `;
-  sectionHome.innerHTML = templateHome;
-  sectionHome.appendChild(navigator);
+  // sectionHome.appendChild(nodo);
   return sectionHome;
+};
+
+export const navigator = () => {
+  const sectionNavigator = document.createElement('div');
+  sectionNavigator.classList.add('navigator');
+  const templateNavigator = `
+    <header>
+        <div class="navLogo">
+          <a href="#/"><img src="./img/logoTuristik.png"></a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#/Home">Home</a></li>
+                <li><a href="#/Profile">Profile</a></li>
+                <li><a href="#/LogOut">LogOut</a></li>
+            </ul>
+        </nav>
+    </header>
+    `;
+  sectionNavigator.innerHTML = templateNavigator;
+  return sectionNavigator;
 };
--- src/view/login.js
+++ src/view/login.js
@@ -10,12 +10,17 @@ export const logInTemplate = () => {
       <input id="emailLogIn" type="email" placeholder="Enter Email" class="inputForm" required>
       <input id="passwordLogIn" type="password" placeholder="Enter Password" class="inputForm" required>
     </form>
-    <button type="submit" class="btnLogIn">Log In</button><br>
+    <button type="button" class="btnLogIn">Log In</button><br>
     <span>New here? <a id="linkRegister" href="#/Register">Register</a></span>
     <div class="divIconG">
       <img src="./img/icons8-logo-de-google.svg" alt="iGoogle" class="iGoogle">
     </div>
     `;
   sectionLogIn.innerHTML = templateLogIn;
+
+  const btnLogIn = sectionLogIn.querySelector('.btnLogIn');
+  btnLogIn.addEventListener('click', () => {
+    console.log('Aqui boton');
+  });
   return sectionLogIn;
 };
