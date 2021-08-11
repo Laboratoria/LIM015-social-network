8dea8748a17a0058089ee0b8d8713f9194660f44
Gina <floresgina2020@gmail.com>
2021-08-10 20:38:41 -0500

    primera parte de autenticacion y css header

--- src/view/home.js
+++ src/view/home.js
@@ -13,17 +13,18 @@ export const navigator = () => {
   const sectionNavigator = document.createElement('div');
   sectionNavigator.classList.add('navigator');
   const templateNavigator = `
-    <header>
-        <div class="navLogo">
-          <a href="#/"><img src="./img/logoTuristik.png"></a>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="#/Home">Home</a></li>
-                <li><a href="#/Profile">Profile</a></li>
-                <li><a href="#/LogOut">LogOut</a></li>
-            </ul>
-        </nav>
+    <header class="header">
+      <nav class="nav">
+        <a href="#/"><img src="./img/logoTuristik.png" class="imageLogo"></a>
+        <button class="navToggle">
+          <i class="fas fa-bars"></i>
+        </button>
+        <ul class="navMenu">
+          <li class="navMenuItem"><a href="#/Home" class="navMenuLink navLink">Home</a></li>
+          <li class="navMenuItem"><a href="#/Profile" class="navMenuLink navLink">Profile</a></li>
+          <li class="navMenuItem"><a href="#/LogOut" class="navMenuLink navLink">LogOut</a></li>
+        </ul>
+      </nav>
     </header>
     `;
   sectionNavigator.innerHTML = templateNavigator;
