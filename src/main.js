1965905171ed90b4f89761ada976306294545b9b
nadia <nadia.chayna.q@gmail.com>
2021-08-11 00:25:44 -0500

    css login/home

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { changeView } from './router.js';
 import { auth } from './scripts/fs-config.js';
 
 const init = () => {
-  // changeView(window.location.hash);
+  changeView(window.location.hash);
   window.addEventListener('hashchange', () => {
     changeView(window.location.hash);
   });
--- src/views/home.js
+++ src/views/home.js
@@ -2,28 +2,33 @@ export default () => {
   const article = document.createElement('article');
   article.className = 'home';
   article.innerHTML = `
-  <section class="top-section">
-      <aside class="home-sidebar">
-        <div class="user-container">
-          <h3>Usuario :3</h3>
-        </div>
-        <div class="animes-container">
-          <h2>Animes Populares:</h2>
-          <div class="posters-container">
-            <h1>snk</h1>
-            <h1>kny</h1>
-            <h1>tr</h1>
-            <h1>jojo's</h1>
-          </div>
-        </div>
-      </aside>
-      <article>
-       <textarea class="form-control" placeholder="¿Qué quieres compartir?"></textarea>
-       <button type="submit" class="btn-share">Compartir</button>
-       <div class="posts">
+  <section class="bottom-section">
+  <aside class="home-sidebar">
+   <div class="user-container">
+     <div class="img">
+       <img class="waifu" src="https://i.ibb.co/vsYdG3b/gfx.jpg" alt=""></div>
+       <div><p>Mima-chan xd</p></div>
        </div>
-      </article>
-  </section>
+    
+     <div class="animes-container">
+     <div class="title-anime">
+      <h2>Animes Populares:</h2></div>
+       <div class="posters-container">
+       <div class="single-image"><a href="#"><img src="https://i.ibb.co/GcqryV3/snk/1.png"></a></div>
+       <div class="single-image"><a href="#"><img src="https://i.ibb.co/4P36SxK/kny/2.png"></a></div>
+       <div class="single-image"><a href="#"><img src="https://i.ibb.co/Cz0H5Ln/tr/3.png"></a></div>
+       <div class="single-image"><a href="#"><img src="https://i.ibb.co/gS7cFf9/jojo/4.png"></a></div>
+    </div>
+  </div>
+</aside>
+<article class="post">
+  <div class="post-txt">
+    <textarea class="txt" rows="6" placeholder="¿Qué quieres compartir?">                      </textarea>
+    <button type="submit" class="btn-share">Compartir</button></div>
+ <div class="posts">
+ </div>
+</article>
+</section>
   `;
   return article;
 };
