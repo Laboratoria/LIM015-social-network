6b6199c1889b4c82732e7d479f1e5ecb596dd5fe
Andrea Blanco <estefania_8_3@hotmail.com>
2021-08-11 07:49:43 -0500

    fix:arregle el error de pagina

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 /* eslint-disable spaced-comment */
 // Este es el punto de entrada de tu aplicacion
 import { changePages } from './routes/router.js';
-import { logueo, loginWithFacebook } from './pages/login.js';
+import { logueo } from './pages/login.js';
 import { register } from './pages/signIn.js';
 
 const init = () => {
   changePages(window.location.hash);
   logueo();
   register();
-  loginWithFacebook();
   window.addEventListener('hashchange', () => {
     logueo();
     register();
-    loginWithFacebook();
     changePages(window.location.hash);
   });
 };
