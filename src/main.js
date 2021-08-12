bc13f7d9310b9656ba7f373c849064148f917a28
Andrea Blanco <estefania_8_3@hotmail.com>
2021-08-12 17:27:12 -0500

    pruebas

--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
 // Este es el punto de entrada de tu aplicacion
-import { changePages } from './routes/router.js';
-
-import firebase from './lib/firebase.js';
-
-const init = () => {
-  changePages(window.location.hash);
-  window.addEventListener('hashchange', () => {
-    changePages(window.location.hash);
-  });
-};
-
-window.addEventListener('load', init);
--- src/pages/error404.js
+++
@@ -1,8 +0,0 @@
-export const notFound = () => {
-  const view = `
-  <p>Oops! page not found</p>
-  `;
-  const divElement = document.createElement('div');
-  divElement.innerHTML = view;
-  return divElement;
-};
