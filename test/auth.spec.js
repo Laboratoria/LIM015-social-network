5247cb58f48e0448b24287574692fc0b5f1d1390
luvale <valeriae200@hotmail.com>
2021-08-14 18:55:57 -0500

    mensajes de error en el form

---
+++ test/auth.spec.js
@@ -0,0 +1,23 @@
+import {
+  signUp, // signIn, googleLogin, logout,
+} from '../src/scripts/fs-login.js';
+
+const firebasemock = require('firebase-mock');
+
+const mockauth = new firebasemock.MockFirebase();
+mockauth.autoFlush();
+
+global.firebase = firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  () => null,
+  () => mockauth,
+);
+
+describe('signUp', () => {
+  it('`signUp` ser una función', () => {
+    expect(typeof signUp).toBe('function');
+  });
+  it('se creó un usuario', () => signUp('hola@gmail.com', '123456').then((userCredential) => {
+    expect(userCredential.user.email).toBe('hola@gmail.com');
+  }));
+});
