40409371d9858569bf5c3adc58a93cf95ad138a9
Andrea Blanco <estefania_8_3@hotmail.com>
2021-08-14 17:47:58 -0500

    feat: agregue la funcion de logOut

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { changeViews } from './routes/router.js';
-import { firebaseWatcher } from './firebase/firebase-auth.js';
 // Este es el punto de entrada de tu aplicacion
 const init = () => {
   changeViews(window.location.hash);
-  firebaseWatcher();
   window.addEventListener('hashchange', () => {
     changeViews(window.location.hash);
-    firebaseWatcher();
   });
 };
 
