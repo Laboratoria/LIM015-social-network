e3253e15c2709d9846ff32d1014faab7de65ca02
luvale <valeriae200@hotmail.com>
2021-08-16 19:06:55 -0500

    correcciones

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -5,10 +5,10 @@ import {
 const firebasemock = require('firebase-mock');
 
 const mockauth = new firebasemock.MockFirebase();
+
 mockauth.autoFlush();
 
 global.firebase = firebasemock.MockFirebaseSdk(
-  // use null if your code does not use RTDB
   () => null,
   () => mockauth,
 );
