d364037981e3688d93166c2964c393a1d0b76855
luvale <valeriae200@hotmail.com>
2021-08-23 10:53:57 -0500

    reorganice las promesas

--- src/scripts/fs-login.js
+++ src/scripts/fs-login.js
@@ -2,62 +2,15 @@ import { auth } from './fs-config.js';
 
 // const usuarios = firebase.firestore().collection('InfoUser');
 // SIGN UP
-export const signUp = (email, password) => {
-  // const userName = document.getElementById('signup-user').value;
-  auth.createUserWithEmailAndPassword(email, password)
-    .then((result) => {
-      const Email = result.user.email;
-      localStorage.setItem('email', Email);
-      console.log('signed up');
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
-    });
-};
+export const signUp = (email, password) => auth.createUserWithEmailAndPassword(email, password);
 
 // SIGN IN
-export const signIn = (email, password) => {
-  auth.signInWithEmailAndPassword(email, password)
-    .then((result) => {
-      const Email = result.user.email;
-      localStorage.setItem('email', Email);
-      console.log('signed in');
-    })
-    .catch((error) => {
-      console.log(error.code, error.message);
-    });
-};
+export const signIn = (email, password) => auth.signInWithEmailAndPassword(email, password);
 
 // GOOGLE LOGIN
 export const googleLogin = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider)
-    .then((result) => {
-      const credential = result.credential;
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      console.log(credential.accessToken);
-      // Envío la información del usuario al LocalStorage
-      const Name = result.user.displayName;
-      const Email = result.user.email;
-      const Photo = result.user.photoURL;
-      const User = {
-        name: Name,
-        email: Email,
-        photo: Photo,
-      };
-      localStorage.setItem('user', JSON.stringify(User));
-      console.log('signed in with Google');
-    })
-    .catch((error) => {
-      console.error('no se pudo iniciar sesión con gugul :c');
-      console.log(error.code);
-      console.log(error.message);
-      // The email of the user's account used.
-      console.log(error.email);
-      // The firebase.auth.AuthCredential type that was used.
-      console.log(error.credential);
-      // ...
-    });
+  return auth.signInWithPopup(provider);
 };
 
 // Log out
