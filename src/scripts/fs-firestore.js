721d570909e44a41d0ec205f54115edceedd0c99
luvale <valeriae200@hotmail.com>
2021-08-24 17:47:34 -0500

    funcion savePost y getPost en proceso

--- src/scripts/fs-firestore.js
+++ src/scripts/fs-firestore.js
@@ -1,13 +1,22 @@
-export const firestore = () => {
-// const db = firebase.firestore();
+const db = firebase.firestore();
 
+const savePost = (post) => {
+  db.collection('posts').doc().set({
+    Post: post.value,
+  });
+};
+
+// const getPost = () => db.collection('posts').doc().get();
+
+export const firestore = () => {
   const mainViews = document.querySelector('.views');
   console.log(mainViews);
 
   mainViews.addEventListener('click', (e) => {
     if (e.target.className === 'post_btn') {
-      const textarea = document.querySelector('.posts').value;
-      console.log(textarea);
+      const post = document.querySelector('.posts');
+      savePost(post);
+      post.value = '';
     }
   });
 };
