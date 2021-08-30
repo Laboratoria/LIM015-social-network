5057631372670a17a9e26854651ee2300e9ee3fe
Andrea Blanco <estefania_8_3@hotmail.com>
2021-08-30 15:06:43 -0500

    chore: corregí algunas cosas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { changeViews } from './routes/router.js';
 // Este es el punto de entrada de tu aplicacion
 const init = () => {
-  changeViews(window.location.hash);
   // window.location.hash = '#/';
+  changeViews(window.location.hash);
   window.addEventListener('hashchange', () => {
     changeViews(window.location.hash);
   });
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,13 @@
 // importamos la funcion que vamos a testear
+import MockFirebase from 'mock-cloud-firestore';
 import { registerWithFirebase } from '../src/firebase/firebase-auth.js';
 
 describe('registerWithFirebase', () => {
-  // beforeEach(() => {
-  //   global.firebase = {
-  //     auth: () => {},
-  //   };
+  beforeEach(() => {
+    global.firebase = {
+      auth: () => { },
+    };
+  });
   it('Deberia ser una función', () => {
     expect(typeof registerWithFirebase).toBe('function');
   });
