837058b73cb9fafcb0472cdaeeeb6076674fb26c
MaffPerez <plzamfr@gmail.com>
2021-09-03 13:01:44 -0500

    Add html

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
+/* eslint-disable no-console */
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const init = () => {
+  window.addEventListener('hashchange', () =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    console.log(window.location.hash));
+};
+
+window.addEventListener('load', init);
