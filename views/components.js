f9fc0c1f4627da9e69076f162e7abe1f6a459573
mysticBel <dnxbel@gmail.com>
2021-09-04 04:32:42 -0500

    adding components.js

---
+++ views/components.js
@@ -0,0 +1,14 @@
+import Login from './login';
+import Register from './register';
+import Community from './community';
+import Profile from './profile';
+
+const components = {
+  login: Login,
+  register: Register,
+  community: Community,
+  profile: Profile,
+
+};
+
+export { components };
