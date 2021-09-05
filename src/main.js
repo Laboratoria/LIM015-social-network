b60d184e916b404c2023ea7149906abfbb3ce967
mysticBel <dnxbel@gmail.com>
2021-09-04 21:52:26 -0500

    fixED ' Error: No Firebase App '[DEFAULT]' has been created ' , now it works !

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { changeView } from './router.js';
+import fsConfig from './scripts/fs-config.js';
 
 const init = () => {
+  fsConfig();
   changeView(window.location.hash);
   window.addEventListener('hashchange', () => changeView(window.location.hash));
 };
