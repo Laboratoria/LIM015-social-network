b1a0a0e4d1901e48e38d5e47e3db129e9bcbc99b
mysticBel <dnxbel@gmail.com>
2021-09-04 19:42:24 -0500

    adding userData + getUserData  in firestore.js

---
+++ src/scripts/firestore.js
@@ -0,0 +1,25 @@
+// (1) User data !
+export const userData = (user) => {
+  const db = firebase.firestore();
+  let Photo;
+  let Name;
+  if (user.photoURL != null && user.displayName != null) {
+    Photo = user.photoURL;
+    Name = user.displayName;
+  } else {
+    Photo = './images/default-avatar.jpg';
+    Name = 'null';
+  }
+  return db.collection('Skyy_Users').doc(user.uid).set({
+    username: Name,
+    email: user.email,
+    photo: Photo,
+    photoCover: Photo,
+    aboutme: 'add about me',
+  });
+};
+
+// (2) get user data
+export const getUserData = (userId) => firebase.firestore().collection('Skyy_Users').doc(userId).get();
+
+// to add  to firestore
