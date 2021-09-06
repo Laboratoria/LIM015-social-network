b10e9898afd5d8e103aace1cfc2baf8bbb934f98
MaffPerez <plzamfr@gmail.com>
2021-09-06 11:39:48 -0500

    Add firebase-config

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC7FHKkQ0uqzB3ZGrplM3XHwgCJWsiWwG0',
+  authDomain: 'onlycats-f46fa.firebaseapp.com',
+  projectId: 'onlycats-f46fa',
+  storageBucket: 'onlycats-f46fa.appspot.com',
+  messagingSenderId: '1070435757718',
+  appId: '1:1070435757718:web:b574ac95f459ba63a5dcb7',
+  measurementId: 'G-MTVZE9ZL82',
+};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 // Este es el punto de entrada de tu aplicacion
 import { changeView } from './view-controler/router.js';
+import { firebaseConfig } from './firebase/firebase-config.js';
 
+firebase.initializeApp(firebaseConfig);
 const init = () => {
   changeView(window.location.hash);
   window.addEventListener('hashchange', () => changeView(window.location.hash));
