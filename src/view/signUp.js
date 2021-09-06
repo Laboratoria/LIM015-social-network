dd1589712599d4f48648a3ec985ea0e52d204f4c
MaffPerez <plzamfr@gmail.com>
2021-09-06 18:12:01 -0500

    add span

--- src/view/signUp.js
+++ src/view/signUp.js
@@ -1,31 +1,37 @@
 export const signUp = () => {
+  const sectionElement = document.createElement('section');
   const viewSignUp = `
     <div  class="home-container">
       <h2>Regístrate</h2>
       <form id="signup-form">
         <div>
-          <input id="signup-username" type="text" placeholder="Nombre de usuario">
+          <input id="signup-username" type="text" placeholder="Nombre de usuario" required>
+          <span id="error-username"></span>
         </div>
         <div>
-          <input id="signup-email" type="text" placeholder="Correo electrónico">
+          <input id="signup-email" type="text" placeholder="Correo electrónico" required>
+          <span id="error-email"></span>
         </div>
         <div>
-          <input id="signup-password" type="password" placeholder="Contraseña">
+          <input id="signup-password" type="password" placeholder="Contraseña" required>
+          <span id="error-password"></span>
         </div>
-        <button id="create-account" type="submit" class="start-button"> Crear cuenta </button>
+        <button id="btn_create-account" type="submit" class="start-button"> Crear cuenta </button>
       </form>
       <ul class="home-list">
         <li>
-          <a href="#/SignIn">¿Tiene cuenta? Inicia con ella</a>
+          <span>¿Tiene cuenta?</span><a href="#/SignIn"> Inicia con ella</a>
         </li>
       </ul>
     </div>`;
-  const sectionElement = document.createElement('section');
+
   sectionElement.classList.add('container-box');
   sectionElement.innerHTML = viewSignUp;
 
   // const signupForm = document.querySelector('#signup-form');
-  // signupForm.addEventListener('submit', (e) => {
+  // const btnCreateAcc = document.getElementById('btn_create-account');
+
+  // btnCreateAcc.addEventListener('submit', (e) => {
   //   e.preventDefault();
   //   console.log('Envia datos');
   // });
