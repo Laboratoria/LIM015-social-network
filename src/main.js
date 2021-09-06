44e85234b9610279407635e63dc55ad2981e060a
ana446 <70743735+ana446@users.noreply.github.com>
2021-09-06 08:53:21 -0500

    b

--- src/main.js
+++ src/main.js
@@ -9,12 +9,6 @@ import { changeView } from './view-controler/router.js';
 //myFunction();
 
 const init = () => {
-<<<<<<< HEAD
-    changeView(window.location.hash)
-window.addEventListener('hashchange', (e) => {
-    e.preventDefault();
-=======
->>>>>>> 8c5fd1e8996fe4252768bd10b204e3fb02f9972e
     changeView(window.location.hash)
     window.addEventListener('hashchange', (e) => {
         e.preventDefault();
