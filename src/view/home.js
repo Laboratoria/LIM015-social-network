2d0af5e40ec1744ad15fc035aebcf222ff90d1c9
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-09-06 11:51:30 -0500

    header

--- src/view/home.js
+++ src/view/home.js
@@ -5,7 +5,61 @@ const viewHome =()=>{
     const htmlHome=
     `<div>
     <nav>${header}</nav> 
-    Este es inicio</div>`;
+    Este es inicio</div>
+    
+    
+    
+<section id="home">
+
+<section id="homeProfile">
+  <div id="people">
+  </div>
+
+  <div  id="name" > ver perfil </div>
+</section>
+
+
+</section id="HomeShare">
+  <div id="imputshare">
+  <input type="text" placeholder="¿ Que desea compartir ?">
+  </div>
+  
+  <button> compartir</button>
+
+<section>
+
+
+</section id="HomeShareEver">
+  <div id="people" ></div>
+
+  <div   id="name" > compartio </div>
+  <div id="dateshare"> </div>
+
+  <div id="imputshare">
+  <input type="text" placeholder="¿ Que desea compartir ?">
+
+  </div>
+
+
+  <div id="likeShare"> guardar</div>
+  <div id="editShare"> editar</div>
+  <div id="removeShare"> eliminar</div>
+
+<section>
+    
+</section>
+    
+    
+    
+    
+    
+    `
+    
+    
+    
+    
+    
+    ;
     
 
     const divHome=document.createElement('div');
