62ebe177e23035e9a4f846a9c8bd102c14b399a6
eliza8855 <elopezvalerin@gmail.com>
2021-09-06 08:47:33 -0500

    configuracion de firebase

---
+++ src/firebase/fb-config.js
@@ -0,0 +1,22 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+
+    // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-auth.js";
+
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDZZ2SUKZwGxxSRp7R-Wmbld97F9bO_m4M",
+    authDomain: "fb-makipura.firebaseapp.com",
+    projectId: "fb-makipura",
+    storageBucket: "fb-makipura.appspot.com",
+    messagingSenderId: "1028709876253",
+    appId: "1:1028709876253:web:22fbbdcc4de54211e17cf4"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
