ba77824bb468119473fe1536a4bc499473489f21
Elizabeth Pinto <elipinto23@gmail.com>
2021-09-06 19:00:19 -0500

    modificando login y home

--- src/view-controller/index.js
+++ src/view-controller/index.js
@@ -20,13 +20,3 @@ const changeView=(route)=>{
 }
 export {changeView}
 
-/* import Home from './home.js';
-import Login from './login.js';
-import Register from './register.js';
-
-const components={
-    home: Home,
-    login: Login,
-    register: Register
-}
-export {components} */
--- src/view/login.js
+++ src/view/login.js
@@ -7,11 +7,11 @@ export default () => {
           <img src="img/pet.png" id="fondoPet" class="fondoAnimalPet">
       <textarea id="name" placeholder="Email" id="email" class="name"></textarea>
       <textarea id="password" placeholder="Password" class="password" id="passwords"></textarea>
-  <input type="button" class="btn">Longin</button>
+    <a href="#/home">Login</a>
     <p>You can also enter with</p>
     <img src="img/face.png" width="40" height="40" class="logoFace">
     <img src="img/gmail.png" width="40px" height="40" class="logoGmail">
-    <p>¿You do not have an account?<a href="#">Sign up</a></p>
+    <p>¿You do not have an account?<a href="#/register">register</a> Sign up</p>
     </div>`;
     const divElem=document.createElement('div')
     divElem.innerHTML =viewLogin;
