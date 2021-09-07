8d91714b34eb557964e5a742f841bf403ace211f
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-09-07 08:48:53 -0500

    setting eslint

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /* Add event of addEventListener / load / hashChange*/
-import{changeView}from './view-controler/route.js'
-import{app} from './db/firebase.js'
+import {changeView} from './view-controler/route.js';
+/* import {app} from './db/firebase.js'; */
 const init = () =>{
     changeView(window.location.hash);
     window.addEventListener('hashchange', () =>
