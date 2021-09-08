3993e276c38eeb6fc8849bbbaa0c706bed8592f3
Elizabeth Pinto <elipinto23@gmail.com>
2021-09-07 19:19:53 -0500

    la primera parte de firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 // Este es el punto de entrada de tu aplicacion
-import{changeView} from './view-controller/index.js';
+import {changeView} from"./view-controller/index.js"
+
+
+const initialization= () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyArJDNhJXShcMtXkp_PsOCvJXz6MDygWvs",
+        authDomain: "pets-lovers-561e8.firebaseapp.com",
+        projectId: "pets-lovers-561e8",
+        storageBucket: "pets-lovers-561e8.appspot.com",
+        messagingSenderId: "392181922252",
+        appId: "1:392181922252:web:1b63378406472e287c8569",
+        measurementId: "G-2KVQK63381"
+      };
+      firebase.initialization(firebaseConfig);
+}
 
 const init =()=>{
     //console.log('init')
--- src/view/login.js
+++ src/view/login.js
@@ -5,15 +5,22 @@ export default () => {
     <h2>Welcome to Pet-Lovers</h2>
     <div class="login">
           <img src="img/pet.png" id="fondoPet" class="fondoAnimalPet">
-      <textarea id="name" placeholder="Email" id="email" class="name"></textarea>
-      <textarea id="password" placeholder="Password" class="password" id="passwords"></textarea>
+      <textarea id="email" placeholder="Email" class="email"></textarea>
+      <textarea id="password" placeholder="Password" class="password" ></textarea>
+      <button type="submit" id="btnsignin" class="signin">signin</button>  
     <a href="#/home">Login</a>
     <p>You can also enter with</p>
     <img src="img/face.png" width="40" height="40" class="logoFace">
     <img src="img/gmail.png" width="40px" height="40" class="logoGmail">
-    <p>¿You do not have an account?<a href="#/register">register</a> Sign up</p>
+    <p>¿You do not have an account?<a href="#/register">register</a></p>
     </div>`;
     const divElem=document.createElement('div')
     divElem.innerHTML =viewLogin;
     return divElem;
 };
+const email= document.querySelector("#email");
+const password= document.querySelector("#password");
+// const buttonSingnin= btnsignin.addEventListener("submit", (e)=>{
+//     e.preventDefault();
+//      console.log(buttonSingnin);
+//  })
