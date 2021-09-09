5a88c49840ffd99800acca1551d650c3ace76fc5
Elizabeth Pinto <elipinto23@gmail.com>
2021-09-09 17:31:17 -0500

    css

--- src/view/login.js
+++ src/view/login.js
@@ -1,28 +1,33 @@
-import { events} from "../firebase/function.js";
+import { events } from "../firebase/function.js";
 
 export default () => {
- 
   const viewLogin = `
+  <form class="contenedorLogin">
     <h1>Pets-Lovers</h1>
-    <h2>Welcome to Pet-Lovers</h2>
     <div class="login">
     <div id="imgLogin">
           <img src="img/pet.png" id="fondoPet" class="fondoAnimalPet">
           </div>
           <div id="info">
       <input id="email" placeholder="Email" class="email">
+      </div>
+      <div id="information">
       <input class="password" type="password" name="password" id="password" placeholder="Password">
+      </div>
+      <div id="btnlogin">
       <button type="submit" id="btnsignin" class="btn">signin</button>  
+      </div>
       </div>
     <a href="#/home">Login</a>
     <p>You can also enter with</p>
     <img src="img/face.png" width="40" height="40" class="logoFace">
     <img src="img/gmail.png" width="40px" height="40" class="logoGmail">
     <p>¿You do not have an account?<a href="#/register">register</a></p>
-    </div>`;
-    const divElem=document.createElement('div')
-    divElem.innerHTML =viewLogin;
-    return divElem;
+    </div>
+    </form>`;
+  const divElem = document.createElement("div");
+  divElem.innerHTML = viewLogin;
+  return divElem;
 };
 //const email= document.querySelector("#email").value;
 //console.log(email);
--- src/view/register.js
+++ src/view/register.js
@@ -1,5 +1,6 @@
 export default () => {
   const viewRegister = `
+  <form class="registerInfo">
       <h1>Pets-Lovers</h1>
       <h2>Welcome to Pet-Lovers</h2>
       <div class="register">
@@ -7,12 +8,23 @@ export default () => {
             <img src="img/animalesFondo.jpg" id="fondo" class="fondoAnimal">
       </div>
       <div id="infoRegister">
+      <div class="registerInformation">
         <input type= "text" id="name1" placeholder="Name"  class="name">
+      </div>
+      <div class="registerInformation">
         <input type= "text" id="email1" placeholder="Email" class="email">
+      </div>
+      <div class="registerInformation">
         <input class="password" type="password" name="password1" id="password1" placeholder="Password">
+      </div>
+      <div class="registerInformation">
         <input class="password" type="password" name="password2" id="password2" placeholder="Password">
+      </div>
+      <div class="regbtn">
         <button id="btnRegister"type="button" class="btn">Register</button>
-      </div>`;
+      </div>
+      </div> 
+    </form> `;
   const divElem = document.createElement("div");
   divElem.innerHTML = viewRegister;
   return divElem;
