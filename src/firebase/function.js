c41900cf99021bced3eaf6e24ca64145c6eb7ed8
Elizabeth Pinto <elipinto23@gmail.com>
2021-09-09 21:16:16 -0500

    El centrado de register

--- src/firebase/function.js
+++ src/firebase/function.js
@@ -1,17 +1,23 @@
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+//import { registerUserNew}from "../firebase/farebase.js";
+// export function resgisterUsuario{
+//     const auth = getAuth();
+//     createUserWithEmailAndPassword(auth, email, password1)
+//       .then((userCredential) => {
+//         if (userCredential==email){
+//             au
+//         }
+//         const user = userCredential.user;
+//         // ...
+//       })
+//       .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         // ..
+//       });
+// }
 
 
-// const auth = getAuth();
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     if(email === userCredential.email)
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
 export function events() {
   const signin = document.querySelector(".btn");
   signin.addEventListener("click", (e) => {
@@ -21,4 +27,3 @@ export function events() {
     console.log(signinEmail, singnPassword);
   });
 }
-
