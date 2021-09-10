0b294eb62c6f442d2825b0d7610e8fe0d3e83781
mysticBel <dnxbel@gmail.com>
2021-09-10 17:34:34 -0500

    now currentUser is working

--- src/views/community.js
+++ src/views/community.js
@@ -1,10 +1,28 @@
-export default () => {
+import { currentUser } from '../scripts/auth.js';
+
+export default (userX) => {
   const viewCommunity = document.createElement('section');
-  viewCommunity.classList.add('container-community');
-  viewCommunity.innerHTML = `
+  const isUser = (user) => {
+    if (user || user !== null) {
+      viewCommunity.classList.add('container-community');
+      viewCommunity.innerHTML = `
           <section>
             <p> main page works ok... 😉</p>
+            <h2 id="name"> ${userX.username}</h2>
           </section>
           `;
+    } else {
+      window.location.hash = '#/';
+    }
+  };
+  currentUser(isUser);
   return viewCommunity;
 };
+
+// var user = firebase.auth().currentUser;
+
+// if (user) {
+// User is signed in.
+// } else {
+// No user is signed in.
+// }
