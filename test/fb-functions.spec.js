c197aa35b2da72a53b9bdba3f4de1a06046bdef2
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-09-12 15:55:22 -0500

    testeo de register y login al 100

--- test/fb-functions.spec.js
+++ test/fb-functions.spec.js
@@ -1,63 +1,59 @@
 /* eslint-disable no-undef */
 
-import {loginEmail,registerEmail /*, loginGoogle,signOut*/} from '../src/firebase/fb-functions.js'
+import {
+  loginEmail,
+  registerEmail,
+  loginGoogle,
+  signOut,
+} from "../src/firebase/fb-functions.js";
 
 // configurando firebase mock
-import firebasemock from 'firebase-mock';
-
+import firebasemock from "firebase-mock";
 
 const mockauth = new firebasemock.MockAuthentication();
 mockauth.autoFlush();
 
-global.firebase =firebasemock.MockFirebaseSdk(
-    // use null if your code does not use RTDB
-    () => null,
-    () => mockauth,
-     //() => mockfirestore,
-  );
+global.firebase = firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  () => null,
+  () => mockauth
+  //() => mockfirestore,
+);
 
- 
-describe('registerEmail',()=>{
- it('craer una cuenta con  correo:elopezvalerin@gmail.com y email:elopezvalerin@gmail.com y contraseña"Nino-124"',()=>{
-     return registerEmail('elopezvalerin@gmail.com','Nino-124').then((user)=>{
-         expect(user.email).toBe('elopezvalerin@gmail.com')
-         expect(user.password).toBe('Nino-124')
-     })
- }) 
-})
+const users = {
+  email: "ben@example.com",
+  password: "examplePass",
+};
 
+describe("registerEmail", () => {
+  it('craer una cuenta con  correo:elopezvalerin@gmail.com y email:elopezvalerin@gmail.com y contraseña"Nino-124"', () => {
+    registerEmail("elopezvalerin@gmail.com", "Nino-124").then((user) => {
+      expect(user.email).toBe("elopezvalerin@gmail.com");
+      expect(user.password).toBe("Nino-124");
+    });
+  });
+});
 
-describe('loginEmail',()=>{
-    it('ingresar a la cuenta con un  correo:.com y email:elopezninos@gmail.com y contraseña"vaca-124"',()=>{
-        return loginEmail('elopezninos@gmail.com','vaca-124').then((user)=>{
-            expect(user.email).toBe('elopezninos@gmail.com')
-            
-        })
-    }) 
-   })
-   
+describe("loginEmail", () => {
+  it('ingresar a la cuenta con un  correo:.com y email:elopezninos@gmail.com y contraseña"vaca-124"', () => {
+    loginEmail("elopezninos@gmail.com", "vaca-124").then((user) => {
+      expect(user.email).toBe("elopezninos@gmail.com");
+    });
+  });
+});
 
+describe("loginGoogle", () => {
+  it("ingresar a la cuenta con una cuenta existente en google de correo:ben@example.com", () => {
+    loginGoogle(users.email, users.password).then((user) => {
+      expect(user).toEqual(users);
+    });
+  });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+describe("signOut", () => {
+  it("permite cerrar sesion de una cuenta abierta", () => {
+    signOut().then((user) => {
+      expect(user).toEqual("llore a mares ");
+    });
+  });
+});
