02e03e9ff2e0bf6bc11ad27af7c6f45dd13136d6
gisselP <https://github.com/gisselP>
2021-09-13 11:48:44 -0500

    update

--- src/view/pageOnlyCats.js
+++ src/view/pageOnlyCats.js
@@ -1,27 +1,34 @@
+/* eslint-disable no-alert */
 import { signOutUser } from '../firebase/firebase-functions.js';
 
 export const pageOnlyCats = () => {
   const pageOcView = `
-  <div class="home-container">
-    <header>
-      <img src="./img/only-cats.png" "alt='only-cats' class="home-img">
+  <div class="page-container">
+    <header class = "header-container">
+      <img src="./img/only-cats.png" "alt='only-cats' class="home-title">
     </header>
-    <aside>
+    <aside class="labels-container">
       <div>Meme</div>
       <div>Meme</div>
       <div>Meme</div>
     </aside>
-    <main>
-      <div>texto publicaion</div>
-      <div>texto publicaion mas respuesta</div>
+    <main class="posts-container">
+      <article>
+        <div>image</div>
+        <div>Nombre de la imagen</div>
+        <textarea class="text-input"></textarea>
+        <div class="published-text"></div>
+        <button class="post-button">Publicar</button>
+      </article>
+      <article>bla bla bla</article>
     </main>
-    <aside>
-      <div class="container">
+    <aside >
+     
         <button class="sign-out"> salir</button>
-      </div>
+     
     </aside>
     <h2> Bienvenidx a OnlyCats </h2> <br>
-      <img src='https://cdn.memegenerator.es/descargar/15315146' width=230px>
+   
   </div>`;
   const sectionElement = document.createElement('section');
   sectionElement.classList.add('container-box');
@@ -34,10 +41,7 @@ export const pageOnlyCats = () => {
         // eslint-disable-next-line no-restricted-globals
         confirm('¿En serio quieres salir?');
         window.location.hash = '#/';
-        console.log('saliste');
-      }).catch((error) => {
-        console.log(error);
-      });
+      }).catch((error) => (error));
   });
   return sectionElement;
 };
