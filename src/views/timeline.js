cfd4a416a5f80a88b2149c2cffc42523d87adfbd
margaZM <aurazambrano227@gmail.com>
2021-09-14 20:27:11 -0500

    Agrega funcionalidad del modal para crear nuevo post

--- src/views/timeline.js
+++ src/views/timeline.js
@@ -7,71 +7,7 @@ export default () => {
             <h3>Categorías</h3>
             <h3>Ver Todos</h3>
         </section>
-        <section class="categories">
-            <figure class="category">
-                <img src="./images/svg/logo-programming" alt="logo-programming">
-                <div class="content-category">
-                    <h4>Programación</h4>
-                    <p class="text-muted">30 post</p>
-                </div>
-            </figure>
-            <figure class="category">
-                <img src="./images/svg/logo-graphic-design" alt="logo-graphic-design">
-                <div class="content-category">
-                    <h4>Programación</h4>
-                    <p class="text-muted">30 post</p>
-                </div>
-            </figure>                             
-            <figure class="category">
-                <img src="./images/svg/logo-education" alt="logo-education">
-                <div class="content-category">
-                    <h4>Programación</h4>
-                    <p class="text-muted">30 post</p>
-                </div>
-            </figure>                              
-            <figure class="category">
-                <img src="./images/svg/logo-programming" alt="logo-programming">
-                <div class="content-category">
-                    <h4>Programación</h4>
-                    <p class="text-muted">30 post</p>
-                </div>
-            </figure>
-            <figure class="category">
-            <img src="./images/svg/logo-programming" alt="logo-programming">
-            <div class="content-category">
-                <h4>Programación</h4>
-                <p class="text-muted">30 post</p>
-            </div>
-            </figure>  
-            <figure class="category">
-            <img src="./images/svg/logo-programming" alt="logo-programming">
-            <div class="content-category">
-                <h4>Programación</h4>
-                <p class="text-muted">30 post</p>
-            </div>
-            </figure>
-            <figure class="category">
-            <img src="./images/svg/logo-programming" alt="logo-programming">
-            <div class="content-category">
-                <h4>Programación</h4>
-                <p class="text-muted">30 post</p>
-            </div>
-            </figure>    
-            <figure class="category">
-            <img src="./images/svg/logo-programming" alt="logo-programming">
-            <div class="content-category">
-                <h4>Programación</h4>
-                <p class="text-muted">30 post</p>
-            </div>
-            </figure>  
-            <figure class="category">
-            <img src="./images/svg/logo-programming" alt="logo-programming">
-            <div class="content-category">
-                <h4>Programación</h4>
-                <p class="text-muted">30 post</p>
-            </div>
-            </figure>  
-        </section>             
+        <section class="categories" id="categories"> </section>             
     </section>
 
     <section class="container-section-post">
@@ -85,7 +21,7 @@ export default () => {
         </section>
 
         <section class="create-publication">
-            <button class="btn-create-post">Crear Publicación <i class="fas fa-plus-circle"></i> </button>
+            <button class="btn btn-create-post">Crear Publicación <i class="fas fa-plus-circle"></i> </button>
         </section>
 
         <section class="container-posts">
@@ -113,24 +49,77 @@ export default () => {
 
     <section class="modal hide">
         <section class="backdrop"> 
-        <section class="content-modal"> 
-            <section class="head-modal">
-                <span> Crear publicación </span>
-                <button class="btn-cerrar-modal"> X </button>
+            <section class="content-modal"> 
+                <section class="head-modal">
+                    <span> </span>
+                    <span> Crear publicación </span>
+                    <button class="btn btn-cerrar-modal"> X </button> 
+                </section>
+                <section class="body-modal">
+                    <img class="avatar avatar-sm" src="" alt="img-user">
+                    <span> Nombre </span> 
+                    
+                    <section class="textarea-group">
+                        <textarea class="textarea-post" id="post-user" row="4" placeholder="¿Qué estás pensando, Nombre?"></textarea>
+                        <i class="far fa-smile"></i>
+                    </section>
+
+                    <section class="emoji-group">
+                        <span id="emoji1"> &#128512; </span> 
+                        <span id="emoji2"> &#128513; </span> 
+                        <span id="emoji3"> &#128514; </span> 
+                        <span id="emoji4"> &#128515; </span> 
+                        <span id="emoji5"> &#128516; </span> 
+                        <span id="emoji6"> &#128517; </span> 
+                        <span id="emoji7"> &#128518; </span> 
+                        <span id="emoji8"> &#128519; </span> 
+                        <span id="emoji9"> &#128520; </span> 
+                        <span id="emoji10"> &#128521; </span> 
+                        <span id="emoji11"> &#128522; </span> 
+                        <span id="emoji12"> &#128523; </span> 
+                        <span id="emoji13"> &#128524; </span> 
+                        <span id="emoji14"> &#128525; </span> 
+                        <span id="emoji15"> &#128526; </span> 
+                        <span id="emoji16"> &#128527; </span> 
+                        <span id="emoji17"> &#128528; </span> 
+                        <span id="emoji18"> &#128529; </span> 
+                        <span id="emoji19"> &#128530; </span> 
+                        <span id="emoji20"> &#128531; </span> 
+                        <span id="emoji21"> &#128532; </span> 
+                        <span id="emoji22"> &#128533; </span> 
+                        <span id="emoji23"> &#128534; </span> 
+                        <span id="emoji24"> &#128535; </span> 
+                        <span id="emoji25"> &#128536; </span> 
+                        <span id="emoji26"> &#128537; </span> 
+                        <span id="emoji27"> &#128538; </span> 
+                        <span id="emoji28"> &#128539; </span> 
+                        <span id="emoji29"> &#128540; </span> 
+                        <span id="emoji30"> &#128541; </span> 
+                        <span id="emoji31"> &#128542; </span> 
+                        <span id="emoji31"> &#128543; </span> 
+                        <span id="emoji31"> &#128544; </span> 
+                        <span id="emoji31"> &#128545; </span> 
+                        <span id="emoji31"> &#128546; </span> 
+                        <span id="emoji31"> &#128547; </span> 
+                        <span id="emoji31"> &#128548; </span> 
+                        <span id="emoji31"> &#128549; </span> 
+                        <span id="emoji31"> &#128550; </span>                     
+                    </section>
+
+                    <section class="reaction-post">
+                        <button class="btn-like" id="share-post"> <i class="far fa-heart"></i> </button>
+                        <button class="btn-comment" id="share-post"> <i class="far fa-comment-dots"></i> </button>
+                        <button class="btn-save" id="share-post"> <i class="far fa-bookmark"></i> </button>
+                    </section>
+                </section>
+            <button class="btn btn-create-post" id="share-post"> Publicar </button>
             </section>
-            <section class="body-modal">
-                <img class="avatar avatar-sm" src="" alt="img-user">
-                <span> Nombre </span> 
-                <textarea id="post-user" placeholder="¿Qué estás pensando, Nombre?"></textarea>
-            </section>
-        </section>
         </section>
     </section>
     `;
     const modal = viewTimeLine.querySelector('.modal');
     const btnCreatePost = viewTimeLine.querySelector('.btn-create-post');
     const btnCerrarModal = viewTimeLine.querySelector('.btn-cerrar-modal');
-
     btnCreatePost.addEventListener('click', openModal);
     btnCerrarModal.addEventListener('click', cerrarModal);
     function openModal() {
@@ -139,5 +128,13 @@ export default () => {
     function cerrarModal() {
         modal.classList.remove('revelar')
     }
+    // //Inserta emoji en textarea
+    const textarea = viewTimeLine.querySelector('.textarea-post');
+    console.log(textarea)
+
+    // textarea.addEventListener('click', () => {
+    //     
+    // })
+
     return viewTimeLine;
 }
