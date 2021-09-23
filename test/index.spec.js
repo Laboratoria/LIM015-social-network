5c921af47c5f57aeeb4652577b2097d009cd7e83
DianaHuaripayta <angelicagonzales1610@gmail.com>
2021-09-23 10:36:17 -0500

    test auth and firestore

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,29 +1,23 @@
 // importamos la funcion que vamos a testear
 /* eslint-disable no-undef */
-import { resetPassword, loginGoogle } from '../src/db/firebase-auth.js';
+import { loginEmail, resetPassword, loginGoogle,registerEmail, signOut } from '../src/db/firebase-auth.js';
 /* eslint-disable no-undef */
+/* trae lo que necesita  */
 const firebasemock = require('firebase-mock');
 const mockauth = new firebasemock.MockFirebase();
-mockauth.autoFlush();
+mockauth.autoFlush();//metodo de mockFirebase
 global.firebase = firebasemock.MockFirebaseSdk(
-  // use null if your code does not use RTDB
   () => null,
   () => mockauth,
 );
-
-describe('resetPassword', () => {
-  it('Deberia enviar un email para restablecer contraseña', () => {
-    const mockSendPasswordResetEmail = jest.fn();
-    firebase.auth().sendPasswordResetEmail = mockSendPasswordResetEmail;
-    resetPassword('test@gmail.com')
-    // verifica si fue llamado el método de firebase
-    expect(mockSendPasswordResetEmail).toHaveBeenCalled();
-    expect(mockSendPasswordResetEmail.mock.calls).toHaveLength(1);
-    // verifica si el metodo recibio como arg el email
-    expect(mockSendPasswordResetEmail).toHaveBeenCalledWith('test@gmail.com');
-  })
-})
-
+/* ---------------loginEmail-------------*/
+describe('login con email y password', () => {
+  it('debería iniciar sesión', () => loginEmail('newuser@gmail.com', '123456')
+    .then((user) => {
+      expect(user.email).toBe('newuser@gmail.com');
+    }));
+});
+/* ---------------login google------------- */
 describe('loginGoogle', () => {
   it('Deberia iniciar sesión con mi cuenta Google', () => { loginGoogle()
     .then((user) => {
@@ -31,5 +25,34 @@ describe('loginGoogle', () => {
     });
   })
 })
+/*--------------registerEmail----------------*/
+describe('crear nuevo usuario', () =>{
+  it('deberia ingresar con la nueva cuenta creada',() => registerEmail('courseShare@gmail.com', '123456')
+  .then((user)=>{
+    expect(user.email).toBe('courseShare@gmail.com')
+  }));
+})
+
+/* --------------resetPassword------------- */
+describe('resetPassword', () => {
+  it('Deberia enviar un email para restablecer contraseña', () => {
+    const mockSendPasswordResetEmail = jest.fn();// jest.fn() - simula una funcion- jest
+    firebase.auth().sendPasswordResetEmail = mockSendPasswordResetEmail;
+    resetPassword('test@gmail.com')
+    // verifica si fue llamado el método de firebase
+    expect(mockSendPasswordResetEmail).toHaveBeenCalled();
+    expect(mockSendPasswordResetEmail.mock.calls).toHaveLength(1);//ver que tiene la propiedad lenght y a demas tiene un valor numerico
+    // verifica si el metodo recibio como arg el email
+    expect(mockSendPasswordResetEmail).toHaveBeenCalledWith('test@gmail.com');
+  })
+})
+/* ---------------signOut-------------- */
+describe('log out',() => {
+  it('it could log out with signOut', () => signOut()
+  .then((user)=>{
+    expect(user).toBe(undefined)
+  }))
+})
+
 
 
