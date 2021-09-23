c5491bd9556600134e23368324dceb66dac5f721
MaffPerez <plzamfr@gmail.com>
2021-09-23 16:07:44 -0500

    Practice

--- src/view/pageOnlyCats.js
+++ src/view/pageOnlyCats.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-alert */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-console */
-import { signOutUser, onAuthStateChanged, currentUser } from '../firebase/firebase-auth.js';
-
+import { signOutUser, onAuthStateChanged } from '../firebase/firebase-auth.js';
 import {
   postCollection, getCollection, deletePost, getUserCollection,
 } from '../firebase/firebase-firestore.js';
@@ -19,9 +18,9 @@ const userStateCheck = () => {
 
 export const pageOnlyCats = () => {
   userStateCheck();
-  const googleUser = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user'));
   const imgDefault = 'https://pbs.twimg.com/profile_images/1101458340318568448/PpkA2kQh_400x400.jpg';
-  const photo = (googleUser.photoURL === null) ? imgDefault : googleUser.photoURL;
+  const photo = (user.photoURL === null) ? imgDefault : user.photoURL;
   const pageOcView = `
   <div class="page-container">
     <header class = "header-container">
@@ -35,107 +34,106 @@ export const pageOnlyCats = () => {
         <button class="label-btn vet">Vet Cat</button>
         <button class="label-btn foodie">Foodie Cat</button>
       </div>
-      <div>
-        <section class="profile-post publish" >
+      <form id="post-form" class="profile-post publish">
           <div class="container-photo">
               <img src=${photo} "alt='picture' class="profile-photo">
           </div>
-          <section class="section-profile" >
-            <textarea class="text-input" id="text-input" placeholder="¿Miau esta pasando?"></textarea>
-            <div class="post-icon">
-              <i class="fas fa-image"></i>
-              <button class="post-button" id="post-button" type="submit">Meow</button>
+            <div class="section-profile" >
+              <textarea class="text-input" id="text-input" placeholder="¿Miau esta pasando?" autofocus></textarea>
+                <div class="post-icon">
+                  <i class="fas fa-image"></i>
+                  <button class="post-button" id="post-button" type="submit">Meow</button>
+                </div>
             </div>
-          </section>
         </section>
         <section  id="other-post">
-        </section>
+      </form>
     </main>
   </div>`;
   const sectionElement = document.createElement('section');
   sectionElement.classList.add('container-box');
   sectionElement.innerHTML = pageOcView;
 
-  const btnPublish = sectionElement.querySelector('#post-button');
-  const textInput = sectionElement.querySelector('#text-input');
-
-  // -------- Crear Posts (C) --------
-  currentUser();
-  const createPost = (e) => {
+  const postForm = sectionElement.querySelector('#post-form');
+  postForm.addEventListener('submit', (e) => {
+    const textInput = sectionElement.querySelector('#text-input');
     e.preventDefault();
-    let userName;
-    console.log(currentUser().email);
-    if (googleUser.displayName === null) {
-      getUserCollection().onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          console.log({ doc });
-          const dataContent = doc.data();
-          console.log({ dataContent });
-          const userNameFirebase = dataContent.usuario;
-          userName = userNameFirebase;
-          console.log(dataContent.email);
-          const post = textInput.value;
-          postCollection(post, userName, photo)
-            .then(() => {
-              textInput.value = ' ';
-            })
-            .catch((error) => {
-              console.error('Error adding document: ', error);
-            });
-        });
+    const post = textInput.value;
+    postCollection(post)
+      .then(() => {
+        console.log('datos guardados');
+        postForm.reset();
+      })
+      .catch((error) => {
+        console.error('Error adding document: ', error);
       });
-      console.log(googleUser.email);
-    } else {
-      userName = googleUser.displayName;
-      const post = textInput.value;
-      postCollection(post, userName, photo)
-        .then(() => {
-          textInput.value = ' ';
-        })
-        .catch((error) => {
-          console.error('Error adding document: ', error);
-        });
-    }
+  });
+  const showPosts = () => {
+    getCollection()
   };
+  // const btnPublish = sectionElement.querySelector('#post-button');
 
-  // -------- Leer Posts (R) --------
-  const readPosts = () => {
-    getCollection().onSnapshot((querySnapshot) => {
-      const newPost = sectionElement.querySelector('#other-post');
-      newPost.innerHTML = ' ';
-      querySnapshot.forEach((doc) => {
-        const dataContent = doc.data();
-        newPost.innerHTML += `
-        <section class="profile-post">
-          <div class="update-post">
-          <button id="btn-deletePost" class="btn-delete" data-id='${doc.id}'>Eliminar</button>
-          <button>Editar</button>
-          </div>
-          <div class="container-photo">
-            <img src="${dataContent.photo}" "alt='picture' class="profile-photo">
-          </div>
-          <section class="section-post">
-            <p class="name-input"> ${dataContent.user} </p>
-            <p readonly class="text-output">${dataContent.text}</p>
-          </section>
-        </section> `;
-      });
-      const btnDelete = sectionElement.querySelectorAll('.btn-delete');
-      btnDelete.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-          deletePost(e.target.dataset.id)
-            .then(() => {
-              console.log('Document successfully deleted!');
-            }).catch((error) => {
-              console.error('Error removing document: ', error);
-            });
-        });
-      });
-    });
-  };
+  // // -------- Crear Posts (C) --------
+  // const createPost = (e) => {
+  //   e.preventDefault();
+  //   let userName;
+  //   if (user.displayName === null) {
+  //     getUserCollection().onSnapshot((querySnapshot) => {
+  //       querySnapshot.forEach(() => {
+  //       });
+  //     });
+  //     console.log(user.email);
+  //   } else {
+  //     userName = user.displayName;
+  //     const post = textInput.value;
+  //     postCollection(post, userName, photo)
+  //       .then(() => {
+  //         textInput.value = ' ';
+  //       })
+  //       .catch((error) => {
+  //         console.error('Error adding document: ', error);
+  //       });
+  //   }
+  // };
 
-  btnPublish.addEventListener('click', createPost);
-  readPosts();
+  // // -------- Leer Posts (R) --------
+  // const readPosts = () => {
+  //   getCollection().onSnapshot((querySnapshot) => {
+  //     const newPost = sectionElement.querySelector('#other-post');
+  //     newPost.innerHTML = ' ';
+  //     querySnapshot.forEach((doc) => {
+  //       const dataContent = doc.data();
+  //       newPost.innerHTML += `
+  //       <section class="profile-post">
+  //         <div class="update-post">
+  //         <button id="btn-deletePost" class="btn-delete" data-id='${doc.id}'>Eliminar</button>
+  //         <button>Editar</button>
+  //         </div>
+  //         <div class="container-photo">
+  //           <img src="${dataContent.photo}" "alt='picture' class="profile-photo">
+  //         </div>
+  //         <section class="section-post">
+  //           <p class="name-input"> ${dataContent.user} </p>
+  //           <p readonly class="text-output">${dataContent.text}</p>
+  //         </section>
+  //       </section> `;
+  //     });
+  //     const btnDelete = sectionElement.querySelectorAll('.btn-delete');
+  //     btnDelete.forEach((btn) => {
+  //       btn.addEventListener('click', (e) => {
+  //         deletePost(e.target.dataset.id)
+  //           .then(() => {
+  //             console.log('Document successfully deleted!');
+  //           }).catch((error) => {
+  //             console.error('Error removing document: ', error);
+  //           });
+  //       });
+  //     });
+  //   });
+  // };
+
+  // btnPublish.addEventListener('click', createPost);
+  // readPosts();
 
   // ------------------ Salir de la página --------------------
   const signOut = sectionElement.querySelector('#sign-out');
