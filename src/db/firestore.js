518f7cfd5b6c73cde2d1fc03571b767d722d374f
margaZM <aurazambrano227@gmail.com>
2021-09-24 21:18:54 -0500

    Corrige error al exportar funcion savePost

--- src/db/firestore.js
+++ src/db/firestore.js
@@ -28,5 +28,5 @@ const updateLikes = (id, totalLikes) => {
     return db.collection('posts').doc(id).update({ totalLikes });
 };
 
-export { saveUser, getAllUsers, getAllCategories, getAllPosts, db, deletePostFs, datePost , storage, updateLikes}
+export { savePost, saveUser, getAllUsers, getAllCategories, getAllPosts, db, deletePostFs, datePost , storage, updateLikes}
 
