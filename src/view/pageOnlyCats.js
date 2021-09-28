9ab8b5a961862edef02d8df0330688efebd5784a
gisselP <https://github.com/gisselP>
2021-09-28 18:47:39 -0500

    add likes function 2

--- src/view/pageOnlyCats.js
+++ src/view/pageOnlyCats.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-console */
 import { signOutUser, onAuthStateChanged } from '../firebase/firebase-auth.js';
 import {
-  postCollection, getCollection, deletePost, getPost, editPost, editHeart,
+  postCollection, getCollection, deletePost, getPost, editPost,
 } from '../firebase/firebase-firestore.js';
 
 const userStateCheck = () => {
@@ -84,8 +84,8 @@ export const pageOnlyCats = () => {
             <p class="name-input"> ${dataContent.user} </p>
             <p readonly class="text-output">${dataContent.text}</p>
             <div class="paw-container">
-              <i class="${dataContent.likes.includes(localUser.uid) ? 'fas' : 'far'} fa-heart" id="${doc.id}"></i>
-              <span>${dataContent.likes.length ? dataContent.likes.length : ''} </span>
+              <i class="far fa-heart" id="${doc.id}"></i>
+              <span> </span>
           </section>
           <div class="update-post  ${(dataContent.email === localUser.email) ? ' ' : 'hide'}">
             <button class="btn-delete"><i class="fas fa-trash" id="${doc.id}"></i></button>
@@ -129,16 +129,8 @@ export const pageOnlyCats = () => {
       const btnHeart = sectionElement.querySelectorAll('.fa-heart');
       btnHeart.forEach((btn) => {
         btn.addEventListener('click', async (e) => {
-          let postSeleccionado = await getPost(e.target.id).then((doc) => doc.data().likes);
-          if (!postSeleccionado.includes(localUser.uid)) {
-            postSeleccionado.push(localUser.uid);
-            await editHeart((e.target.id), { likes: postSeleccionado });
-            console.log('si le diste likee');
-          } else {
-            postSeleccionado = postSeleccionado.filter((lik) => lik !== localUser.uid);
-            await editHeart((e.target.id), { likes: postSeleccionado });
-            console.log('todavia no le has dado like');
-          }
+          const postSeleccionado = await getPost(e.target.id);
+          console.log(postSeleccionado.data());
         });
       });
     });
