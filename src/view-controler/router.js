feb2e3d722eaad9b3bb5ed40c97cf54148f15a12
eliza8855 <elopezvalerin@gmail.com>
2021-09-29 15:17:04 -0500

    ocultar vistar segun el estado del usuario

--- src/view-controler/router.js
+++ src/view-controler/router.js
@@ -2,27 +2,49 @@ import { components } from '../view/indexView.js'
 
 
 const changeView=(route) => {
-  // firebase.auth().onAuthStateChanged((user) => {
-    
-    console.log(route);
     const container = document.getElementById("container");
     container.innerHTML='';
     switch (route){
       case '': 
       case '#':
-      case '#/': { container.appendChild(components.login())}
+      case '#/':
+      firebase.auth().onAuthStateChanged((user) => {
+          if(user){
+            if(user.emailVerified){window.location.replace('#/home')}
+            else{container.appendChild(components.login())}
+          }
+        else{container.appendChild(components.login())}
+        })
       break;      
-      case '#/register':{container.appendChild(components.register())}
+      case '#/register': 
+      firebase.auth().onAuthStateChanged((user) => {
+        if(user){
+            if(user.emailVerified){window.location.replace('#/home')}
+            else{container.appendChild(components.register())}
+          }
+        else{container.appendChild(components.register())}
+        })
       break;
-      case '#/home':{ container.appendChild(components.header()); container.appendChild(components.home()); container.appendChild(components.footer())}
-                  // else{window.location.replace('#')}
+      case '#/home':firebase.auth().onAuthStateChanged((user) => {
+        if(user){
+          if(user.emailVerified){container.appendChild(components.header()); container.appendChild(components.home()); container.appendChild(components.footer())}
+          else{window.location.replace('#')}
+        }
+        else{window.location.replace('#')}
+      })    
       break;
-      case '#/profile':{ container.appendChild(components.header()); container.appendChild(components.profile()) }
-      break;
+      case '#/profile':firebase.auth().onAuthStateChanged((user) => {
+        if(user){
+          if(user.emailVerified){container.appendChild(components.header()); container.appendChild(components.profile())}
+          else{window.location.replace('#')}
+        }
+        else{window.location.replace('#')}
+      })
+      break; 
       default: 
       container.appendChild(components.Err404())  
       }
-    // })
+  
 }
 
 export  {changeView}
