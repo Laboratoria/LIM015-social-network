9461c4deddfb6a3001f9c5d84acd4788561c3a7d
Elizabeth Pinto <elipinto23@gmail.com>
2021-09-29 22:47:02 -0500

    ya se hicieron los test de login y register

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,6 @@
-import { registerWithEmail } from '../src/security/security.function.js';
+import {
+  registerWithEmail, logInWithEmail, logInWithGoogle,
+} from '../src/security/security.function.js';
 
 const firebasemock = require('firebase-mock');
 
@@ -10,9 +12,27 @@ global.firebase = new firebasemock.MockFirebaseSdk(
   () => null,
   () => mockauth,
 );
-
+// ----------------------- register -----------------------//
 describe('registerWithEmail', () => {
-  it('Se registre con correo y contraseña', () => {
-    expect(typeof registerWithEmail).toBe('function');
+  it('deberia ser una funcion', () => { expect(typeof registerWithEmail).toBe('function'); });
+  it('Debería poder registrarse con correo y contraseña', () => registerWithEmail('net@gmail.com', '12345678')
+    .then((user) => {
+      expect(user.email).toBe('net@gmail.com');
+    }));
+});
+// ----------------------- inicion de sesion -----------------------//
+describe('iniciar sesion correo, contraseña', () => {
+  it('deberia iniciar sesion', () => logInWithEmail('pepito@gmail.com', '123456')
+    .then((user) => {
+      expect(user.email).toBe('pepito@gmail.com');
+    }));
+});
+
+describe('logInWithGoogle', () => {
+  it('deberia ser una funcion', () => { expect(typeof logInWithGoogle).toBe('function'); });
+});
+it('debería iniciar sesión con Google', () => {
+  logInWithGoogle().then((google) => {
+    expect(google.providerData[0].providerId).toBe('google.com');
   });
 });
