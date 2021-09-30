0bb0c3c7b1fedf85bc941c29d6320a147164e903
Elizabeth Pinto <elipinto23@gmail.com>
2021-09-29 22:14:39 -0500

    ya funciona farebasemock

--- src/security/firebase.js
+++ src/security/firebase.js
@@ -1,14 +1,10 @@
-export const initialization = () => {
-  const firebaseConfig = {
-    apiKey: 'AIzaSyArJDNhJXShcMtXkp_PsOCvJXz6MDygWvs',
-    authDomain: 'pets-lovers-561e8.firebaseapp.com',
-    projectId: 'pets-lovers-561e8',
-    storageBucket: 'pets-lovers-561e8.appspot.com',
-    messagingSenderId: '392181922252',
-    appId: '1:392181922252:web:1b63378406472e287c8569',
-    measurementId: 'G-2KVQK63381',
-  };
-  firebase.initializeApp(firebaseConfig);
+const firebaseConfig = {
+  apiKey: 'AIzaSyArJDNhJXShcMtXkp_PsOCvJXz6MDygWvs',
+  authDomain: 'pets-lovers-561e8.firebaseapp.com',
+  projectId: 'pets-lovers-561e8',
+  storageBucket: 'pets-lovers-561e8.appspot.com',
+  messagingSenderId: '392181922252',
+  appId: '1:392181922252:web:1b63378406472e287c8569',
+  measurementId: 'G-2KVQK63381',
 };
-initialization();
-export const auth = firebase.auth();
+firebase.initializeApp(firebaseConfig);
