9bc4bedaf704d0d70ec990da24e76346a15392ec
yovana888 <yovana.otaku@gmail.com>
2021-10-01 20:47:01 -0500

    update para resolver conflicto

--- src/views/timeline/viewCategories.js
+++ src/views/timeline/viewCategories.js
@@ -1,5 +1,5 @@
-import { getAllCategories,getPostByCategory } from '../../db/firestore.js';
-import {loadViewPost} from './viewPosts.js';
+import { getAllCategories, getPostByCategory } from '../../db/firestore.js';
+import { loadViewPost } from './viewPosts.js';
 import { alerts } from '../../lib/alerts.js';
 const allCategories = () => {
     const objectCategories = [];
@@ -25,75 +25,73 @@ const loadViewCategory = async() => {
         const figureCategory = document.createElement('figure');
         figureCategory.classList.add('category');
         figureCategory.innerHTML = `
-       <img src="../../images/svg/categorias/${ element.imagen }" alt="logo-categories">
-       <div class="content-category">
-           <span class="text-category categoryName" id="textCategory-${element.idCategory}" data-id=${element.idCategory} data-total=${element.totalPosts}> ${ element.nameCategory } </span>
-           <p class="text-muted" id="category-${element.idCategory}">  ${ element.totalPosts } post</p>
-       </div>
-     `
+        <img src="../../images/svg/categorias/${ element.imagen }" alt="logo-categories">
+        <div class="content-category">
+            <span class="text-category categoryName" id="textCategory-${element.idCategory}" data-id=${element.idCategory} data-total=${element.totalPosts}> ${ element.nameCategory } </span>
+            <p class="text-muted" id="category-${element.idCategory}">  ${ element.totalPosts } post</p>
+        </div>
+        `
         categories.appendChild(figureCategory);
-    })
+    });
 }
-const addEventShowCategories=()=>{
+const addEventShowCategories = () => {
     const allCategoriesName = document.querySelectorAll('.categoryName');
     allCategoriesName.forEach(span => {
-        span.addEventListener('click',async(e) =>{
+        span.addEventListener('click', async(e) => {
             const idSpanCategory = e.target.dataset.id;
             const totalPosts = e.target.dataset.total;
-            const spanCategory = document.querySelector('#textCategory-'+ idSpanCategory);
+            const spanCategory = document.querySelector('#textCategory-' + idSpanCategory);
             const textCategory = spanCategory.innerHTML;
             const objectPosts = [];
-            const idUserAuth = localStorage.getItem('iduser'); 
-            const allUsers = JSON.parse(window.localStorage.getItem('allUsers')); 
+            const idUserAuth = localStorage.getItem('iduser');
+            const allUsers = JSON.parse(window.localStorage.getItem('allUsers'));
             await getPostByCategory(idSpanCategory)
-            .then((querySnapshot)=>{
+                .then((querySnapshot) => {
 
-                querySnapshot.forEach((doc) =>{
-                    const userByPost = allUsers.find(element => element.idUser === doc.data().idUser);
-                    objectPosts.push({
-                        idPost: doc.id,
-                        idUser: doc.data().idUser,
-                        nameUser: userByPost.nameUser,
-                        photoUser: userByPost.photoUser,
-                        contentPost: doc.data().contentPost,
-                        datePost: doc.data().datePost.toDate().toDateString(),
-                        nameImage: doc.data().nameImage,
-                        arrLikes: doc.data().arrLikes,
-                        publicPosts: doc.data().publicPosts,
-                        totalComments: doc.data().totalComents,
-                        image: doc.data().image,
-                        idCategory: doc.data().idCategory,
-                        nameCategory: textCategory,
-                        urlImage: doc.data().urlImage,
-                        arrComments: doc.data().arrComments
+                    querySnapshot.forEach((doc) => {
+                        const userByPost = allUsers.find(element => element.idUser === doc.data().idUser);
+                        objectPosts.push({
+                            idPost: doc.id,
+                            idUser: doc.data().idUser,
+                            nameUser: userByPost.nameUser,
+                            photoUser: userByPost.photoUser,
+                            contentPost: doc.data().contentPost,
+                            datePost: doc.data().datePost.toDate().toDateString(),
+                            nameImage: doc.data().nameImage,
+                            arrLikes: doc.data().arrLikes,
+                            publicPosts: doc.data().publicPosts,
+                            totalComments: doc.data().totalComents,
+                            image: doc.data().image,
+                            idCategory: doc.data().idCategory,
+                            nameCategory: textCategory,
+                            urlImage: doc.data().urlImage,
+                            arrComments: doc.data().arrComments
 
+                        })
                     })
-                })
 
-               if (objectPosts.length == 0) {
-                alerts('info', 'No hay posts para mostrar' )
-               }else{
-                console.log('soy diferente de 0')
-                const dataPublic = objectPosts.filter(element => element.publicPosts == 'true' || element.idUser == idUserAuth);
-                const containerPost = document.querySelector('#container-posts');
-                if (dataPublic.length == 0) {
-                    alerts('info', 'Los '+ objectPosts.length +' posts son privados.' )
-                }else{
-                    const totalPrivate = parseInt( totalPosts ) - dataPublic.length;
-                    containerPost.innerHTML="";
-                    loadViewPost(dataPublic, containerPost);
-                    if (totalPrivate > 0) { //esto quiere decir que total de posts puede que halla mas de privado.
-                        alerts('info', 'No se mostraran '+ totalPrivate + ' por que son privados');
+                    if (objectPosts.length == 0) {
+                        alerts('info', 'No hay posts para mostrar')
+                    } else {
+                        const dataPublic = objectPosts.filter(element => element.publicPosts == 'true' || element.idUser == idUserAuth);
+                        const containerPost = document.querySelector('#container-posts');
+                        if (dataPublic.length == 0) {
+                            alerts('info', 'Los ' + objectPosts.length + ' posts son privados.')
+                        } else {
+                            const totalPrivate = parseInt(totalPosts) - dataPublic.length;
+                            if (totalPrivate > 0) { //esto quiere decir que total de posts puede que halla mas de privado.
+                                alerts('info', 'No se mostraran ' + totalPrivate + ' por que son privados');
+                            }
+                            containerPost.innerHTML = "";
+                            loadViewPost(dataPublic, containerPost);
+                        }
                     }
-                   
-                }
-               }
-                
-                
-            })
+
+
+                })
         })
     });
-    
+
 }
 
-export { loadViewCategory,addEventShowCategories }
+export { loadViewCategory, addEventShowCategories }
