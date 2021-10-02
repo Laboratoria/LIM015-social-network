692efcab28ea590ae094cf15a644fd6473c6c3d2
TaniQL <taniaquispelazo@gmail.com>
2021-10-01 20:29:57 -0500

    Post Image & Text

--- src/firebase/firebase-storage.js
+++ src/firebase/firebase-storage.js
@@ -2,3 +2,5 @@ export const uploadPostImage = (name, photo) => firebase.storage()
   .ref().child(`posts/${name}`).put(photo);
 export const getPostImageURL = (dir, name) => firebase.storage()
   .refFromURL(`gs://onlycats-f46fa.appspot.com/${dir}/${name}`).getDownloadURL();
+export const deletePostImage = (name, photo) => firebase.storage()
+  .ref().child(`posts/${name}`).delete(photo);
